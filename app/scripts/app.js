869860fee9a1b790e33991d1b0ac3ad022005b60
Stephen Smith <stephen304@gmail.com>
2016-01-23 13:28:59 -0500

    Initial commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name uadpApp
+ * @description
+ * # uadpApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('uadpApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngMessages',
+    'ngResource',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uadpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the uadpApp
+ */
+angular.module('uadpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
