c58311b0fab47c765db49effd01bdfe1fdcc2390
Stephen Smith <stephen304@gmail.com>
2016-01-23 18:59:41 -0500

    feat: Save jurisdiction to local storage.

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -14,16 +14,22 @@ angular.module('uadpApp')
         .get(cfg.apiUrl + '/login?username=' + encodeURIComponent($scope.user.username) + '&password=' + encodeURIComponent($scope.user.password))
         .success(function (data, status, headers, config) {
           $window.localStorage.token = data.sessionToken;
+          $window.localStorage.jurisdiction = data.jurisdictionInfo;
           $state.go('submit');
         })
         .error(function (data, status, headers, config) {
           // Erase the token if the user fails to log in
           delete $window.localStorage.token;
+          delete $window.localStorage.jurisdiction;
 
           // Handle login errors here
           $scope.message = 'Error: Invalid user or password';
         });
     };
+
+    if ($window.localStorage.token) {
+      $state.go('submit');
+    }
 
     this.awesomeThings = [
       'HTML5 Boilerplate',
