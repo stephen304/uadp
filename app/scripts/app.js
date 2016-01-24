93f6e1d46d5f0684403d9839d2ab8427912b026e
Stephen Smith <stephen304@gmail.com>
2016-01-24 04:11:05 -0500

    feat: Add prompt to reload when app is updated.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -41,7 +41,19 @@ angular
   .constant('cfg', {
     apiUrl: 'https://api.parse.com/1'
   })
-  .run(function($http) {
+  .run(function($http, $window) {
     $http.defaults.headers.common["X-Parse-Application-Id"] = "cX2Xh7gf3NiSl9hKIzVggArDILe2LulrjHdhZTF6";
     $http.defaults.headers.common["X-Parse-REST-API-Key"] = "PR6moiFgsmZzX6a4SRqLhk0ebc8cLmzYgBDkIfHO";
+
+    // Check if a new cache is available on page load.
+    $window.addEventListener('load', function(e) {
+      $window.applicationCache.addEventListener('updateready', function(e) {
+        if ($window.applicationCache.status == $window.applicationCache.UPDATEREADY) {
+          // Browser downloaded a new app cache.
+          if (confirm('A new version of this site is available. Load it?')) {
+            $window.location.reload();
+          }
+        }
+      }, false);
+    }, false);
   });
