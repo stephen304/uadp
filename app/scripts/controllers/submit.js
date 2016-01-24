d6ef995cd8daea27f1efa4da111dc77e2f56d1a0
johndeyrup <john.deyrup@gmail.com>
2016-01-23 23:02:25 -0500

    added all values to form

--- app/scripts/controllers/submit.js
+++ app/scripts/controllers/submit.js
@@ -14,10 +14,56 @@ angular.module('uadpApp')
       'AngularJS',
       'Karma'
     ];
-    var mydate = new Date();
-    $scope.form = {"date": mydate};
+
+    //set form date to current date, and set forms user, location, 
+    /*var mydate = new Date();
+    $scope.form["123"] = "1234";*/
+    $scope.form = {};
+
+    //define select options for age group
+    $scope.ageOptions = [
+      {value: "Under 18", name: 'Under 18'},
+      {value: "18 to 30", name: "18 to 30"},
+      {value: "31 to 40", name: "31 to 40"},
+      {value: "41 to 50", name: "41 to 50"},
+      {value: "51 to 60", name: "51 to 60"},
+      {value: "Over 60", name: "Over 60"}
+    ];
+
+    $scope.raceOptions = [
+      {value: "Caucasian", name: "Caucasian"},
+      {value: "Hispanic", name: "Hispanic"},
+      {value: "African American", name: "African American"},
+      {value: "Asian", name: "Asian"},
+      {value: "Other", name: "Other"}
+    ];
+
+    $scope.recordOptions = [
+      {value: "true", name: "Yes"},
+      {value: "false", name: "No"}
+    ];
+
+    $scope.maritalOptions = [
+      {value: "Married", name: "Married"},
+      {value: "In a relationship", name: "In a relationship"},
+      {value: "Divorced", name: "Divorced"},
+      {value: "Separated", name: "Separated"},
+      {value: "Single", name: "Single"}
+    ];
+
+    $scope.educationOptions = [
+      {value: "No HS Diploma or GED", name: "No HS Diploma or GED"},
+      {value: "HS diploma or GED", name: "HS diploma or GED"},
+      {value: "College degree", name: "College degree"},
+      {value: "Graduate School", name: "Graduate School"}
+    ];
+
 
     $scope.submit = function() {
+        $scope.form["locationID"] = "41.7,87.7";
+        $scope.form["offenderFacePhoto"] = "";
+        $scope.form["offenderIDPhoto"] = "";
+        $scope.form["offenderVehiclePhoto"] = "";
     	console.log($scope.form);
     	$http
         .post(cfg.apiUrl + "/classes/" + $window.localStorage.jurisdiction, $scope.form)
