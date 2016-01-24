e545b14ea6844f3ad2e9cc21d8c5b46a5161f450
johndeyrup <john.deyrup@gmail.com>
2016-01-24 14:54:07 -0500

    Changed url to config url in record.js

--- app/scripts/controllers/records.js
+++ app/scripts/controllers/records.js
@@ -8,14 +8,14 @@
  * Controller of the uadpApp
  */
 angular.module('uadpApp')
-  .controller('RecordsCtrl', function ( $scope, $http, $window) {
+  .controller('RecordsCtrl', function ( $scope, $http, $window, cfg) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
     $http
-    	.get('https://api.parse.com/1/classes/US_IL_CookCounty')
+    	.get(cfg.apiUrl + "/classes/US_IL_CookCounty")
     	.success(function (data, status, headers, config) {
           $scope.myTable = data.results;
           console.log(data.results);
