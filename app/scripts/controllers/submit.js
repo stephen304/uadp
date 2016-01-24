a958301daa120a58a43ba3e3faf8bad8aa241b5d
johndeyrup <john.deyrup@gmail.com>
2016-01-23 19:09:10 -0500

    added submit so can post form

--- app/scripts/controllers/submit.js
+++ app/scripts/controllers/submit.js
@@ -8,12 +8,34 @@
  * Controller of the uadpApp
  */
 angular.module('uadpApp')
-  .controller('SubmitCtrl', ['$scope', function ($scope) {
+  .controller('SubmitCtrl', function (cfg, $scope, $http, $window, $state) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
     var mydate = new Date();
-    $scope.date = mydate;
-  }]);
+    $scope.form = {"date": mydate};
+
+    $scope.submit = function() {
+    	console.log($scope.form);
+    	$http
+        .post(cfg.apiUrl + "/classes/" + $window.localStorage.jurisdiction, $scope.form)
+        .success(function (data, status, headers, config) {
+          $window.localStorage.token = data.sessionToken;
+          $state.go('submit');
+        })
+        .error(function (data, status, headers, config) {
+          // Erase the token if the user fails to log in
+          delete $window.localStorage.token;
+
+          // Handle login errors here
+          $scope.message = 'Error: Invalid user or password';
+        });
+    };
+
+    if(!$window.localStorage.token) {
+    	$state.go('login');
+    }
+
+  });
